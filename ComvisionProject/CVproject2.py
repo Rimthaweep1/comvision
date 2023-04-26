450876b9fc8817301a1a3430399c6c765865b7ee
Rimthaweep1 <89637616+Rimthaweep1@users.noreply.github.com>
2023-04-26 07:48:36 +0700

    Update CVproject2.py

--- ComvisionProject/CVproject2.py
+++ ComvisionProject/CVproject2.py
@@ -11,18 +11,19 @@ model.iou = 0.45
 model.multi_label = False
 model.max_det = 1000
 
-    # detection part
-video = cv2.VideoCapture(0) # Read USB Camera
+    # detection and show part
+ # Read webcame
+video = cv2.VideoCapture(0) 
 while(video.isOpened()):
-        # Read Frame
         ret, frame = video.read()
         if not ret:
-            print('Reached the end of the video!')
+            print('Error cant find Webcam')
             break
+         #Because yolov5 give score in RGB
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = model(frame)
-        cv2.imshow('Object detector', cv2.cvtColor(results.render()[0], cv2.COLOR_RGB2BGR))
-        # Press 'q' to quit
+        cv2.imshow('Object detector', cv2.cvtColor(results.render()[0], cv2.COLOR_RGB2BGR))#Change back to BGR
+         # Press 'q' to quit
         if cv2.waitKey(1) == ord('q'): 
             break
 video.release()
